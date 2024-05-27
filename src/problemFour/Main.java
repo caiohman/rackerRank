f5a8e46c45da82073d192c722562eb53c4a43cf7
caiohman <cbalthazar70@gmail.com>
2024-05-27 00:22:34 -0300

    readInt and readline problem

--- src/problemFour/Main.java
+++ src/problemFour/Main.java
@@ -1,4 +1,21 @@
 package problemFour;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        Integer number = scanner.nextInt();
+        Double numberDouble = scanner.nextDouble();
+        scanner.nextLine();
+        String text = scanner.nextLine();
+
+        System.out.println("String: " + text);
+        System.out.println("Double: " + numberDouble);
+        System.out.println("Int: " + number);
+
+    }
 }
