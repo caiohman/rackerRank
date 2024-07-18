3ef5410152c9ad96de11960e76a2b3372a7c3226
caiohman <cbalthazar70@gmail.com>
2024-07-17 22:50:58 -0300

    substring example

--- sql/Ten.sql
+++ sql/Ten.sql
@@ -2,4 +2,13 @@
 -- Your result cannot contain duplicates.
 -- Table STATION
 -- Fields (ID, CITY, STATE, LAT_N, LONG_W)
+select distinct s.city from station s 
+where substring(s.city, 1, 1) = 'a' or substring(s.city, 1, 1) = 'A' or
+ substring(s.city, 1, 1) = 'e' or substring(s.city, 1, 1) = 'E' or 
+ substring(s.city, 1, 1) = 'i' or substring(s.city, 1, 1) = 'I' or
+ substring(s.city, 1, 1) = 'o' or substring(s.city, 1, 1) = 'O' or
+ substring(s.city, 1, 1) = 'u' or substring(s.city, 1, 1) = 'U';
+
+
+
 
